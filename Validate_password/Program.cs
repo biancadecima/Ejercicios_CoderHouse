ce0f48ea7decae0ee92d3f734d672bf0062140e2
Bianca <bianbdecima@gmail.com>
2023-01-23 11:56:54 -0300

    Commit Validate_password
    Subiendo proyecto de validar contraseña

---
+++ Validate_password/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+int counter = 5;
+const string password = "coder2022";
+
+while (counter != 0)
+{
+    Console.WriteLine("Ingrese la contraseña (tiene " + counter + " intentos)");
+    string savedPassword = Console.ReadLine();
+    if (savedPassword != null)
+    {
+        if (validatePasswordLength(savedPassword))
+        {
+            if (password == savedPassword)
+            {
+                string hiddenPassword;
+                hiddenPassword = hidePassword(savedPassword);
+                Console.WriteLine(hiddenPassword + " es correcta");
+                counter = 0;
+            }
+            else
+            {
+                Console.WriteLine("Contraseña errónea");
+                counter--;
+            }
+        }
+        else
+        {
+            Console.WriteLine("Contraseña inválida por tener menos de 5 dígitos");
+            counter--;
+        }
+    }
+
+
+}
+Console.WriteLine("Finalización del programa.");
+
+bool validatePasswordLength(string password)
+{
+    if (password.Length >= 5)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+string hidePassword(string password)
+{
+    string hash = "";
+    for (int i = 0; i < password.Length; i++)
+    {
+        hash += "*";
+    }
+    return hash;
+}
